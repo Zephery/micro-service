e8052f9e7c8f2035baf6dacf53c3c15b01306058
Zephery <1570631036@qq.com>
2018-03-17 01:35:29 +0800

    lua

--- conf/lua/hello.lua
+++ conf/lua/hello.lua
@@ -3,8 +3,21 @@ local red = redis:new()
 
 local request_uri = ngx.var.request_uri
 
+local function close_redis(red)
+    if not red then
+        return
+    end
+    local pool_max_idle_time = 10000
+    local pool_size = 100
+    red:set("pool_size", pool_size)
+    local ok, err = red:set_keepalive(pool_max_idle_time, pool_size)
+    if not ok then
+        ngx_log(ngx_ERR, "set redis keepalive error : ", err)
+    end
+end
+
 if (request_uri == "/" or request_uri == "/index.html") then
-    red:set_timeout(1000) -- 1 sec
+    red:set_timeout(1000)
     red:connect("119.23.46.71", 6340)
     local ok, err = red:auth("root")
     if not ok then
@@ -18,8 +31,10 @@ if (request_uri == "/" or request_uri == "/index.html") then
     if resp == ngx.null then
         resp = "<h1>hello world</h1>"
     end
+    red:set("shouye", "hello world")
+    red:expire("shouye", "32")
     ngx.print(resp)
-    red:close()
+    close_redis(red)
     return
 end
 local pagenum = ngx.req.get_uri_args()["pagenum"]
