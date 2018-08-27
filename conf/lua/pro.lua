09de2df5afffd71f3f3905d013978701dccc5016
wenzhihuai <1570631036@qq.com>
2018-08-27 22:39:45 +0800

    update

--- conf/lua/pro.lua
+++ conf/lua/pro.lua
@@ -38,7 +38,8 @@ if resp == ngx.null then
     red:set("targetURL", targetURL)
     local respp = ngx.location.capture(targetURL, { method = ngx.HTTP_GET, args = uri_args })
     red:set(key, respp.body)
-    red:expire(key,3600)
+    ngx.print(respp.body)
+    red:expire(key, 3600)
     return
 end
 ngx.print(resp)
